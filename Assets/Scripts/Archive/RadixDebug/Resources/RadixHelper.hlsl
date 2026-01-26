e63bea9e7149edc62e542fe541cd4b2a52392208
PepperShaker <martherssteve@gmail.com>
2026-01-26 17:15:15 +0800

    update

---
+++ Assets/Scripts/Archive/RadixDebug/Resources/RadixHelper.hlsl
@@ -0,0 +1,13 @@
+#define RADIX_R 16
+#define THREAD_NUM_X 1024
+
+uint get4Bits(uint num, int i)
+{
+    // i is current iteration (pass)
+    return ((num >> i*4) & 0xf);
+}
+
+uint4 get4Bits(uint4 num,int i)
+{
+    return ((num >> i*4) & 0xf);
+}
